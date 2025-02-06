44fd9779c4039aad7d18d38e033012c0cd73d5fb
Eshtiaque <est.ahmed111@gmail.com>
2025-02-07 01:24:32 +0600

    Done

--- src/Components/Invoice.jsx
+++ src/Components/Invoice.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import img from '../assets/nyntax.png'
 
+//sajid 
+
 const Invoice = () => {
   const location = useLocation();
   const invoiceData = location.state.invoiceData;
